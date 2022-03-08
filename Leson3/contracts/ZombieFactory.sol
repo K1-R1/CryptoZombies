e8eb373ee63301030b913d8ac70bb4ab63643c3a
K1-R1 <kyranrawlinson@gmail.com>
2022-03-08 19:26:14 +0000

    Lesson 3, chapter 2

--- Leson3/contracts/ZombieFactory.sol
+++ Leson3/contracts/ZombieFactory.sol
@@ -2,7 +2,9 @@
 
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
     event NewZombie(uint256 zombieId, string name, uint256 dna);
 
     uint256 dnaDigits = 16;
