b55b1a20cb2563f262db9cc5cc48934598c16b63
K1-R1 <kyranrawlinson@gmail.com>
2022-03-08 17:38:33 +0000

    Lesson 1, chapter 5

--- Lesson1/contracts/ZombieFactory.sol
+++ Lesson1/contracts/ZombieFactory.sol
@@ -5,4 +5,9 @@ pragma solidity >=0.5.0 <0.6.0;
 contract ZombieFactory {
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
 }
