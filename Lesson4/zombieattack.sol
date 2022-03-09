2b66bfd8881c051d46a85dcea6d3fda89a38093e
K1-R1 <kyranrawlinson@gmail.com>
2022-03-09 18:06:23 +0000

    Lesson 4, chapter 5

--- Lesson4/zombieattack.sol
+++ Lesson4/zombieattack.sol
@@ -6,6 +6,7 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
     uint256 randNonce = 0;
+    uint256 attackVictoryProbability = 70;
 
     function randMod(uint256 _modulus) internal returns (uint256) {
         randNonce++;
@@ -13,4 +14,6 @@ contract ZombieAttack is ZombieHelper {
             uint256(keccak256(abi.encodePacked(now, msg.sender, randNonce))) %
             _modulus;
     }
+
+    function attack(uint256 _zombieId, uint256 _targetId) external {}
 }
