8dcf70a0b0bce083c0189b3d9ed146719aaac3a8
K1-R1 <kyranrawlinson@gmail.com>
2022-03-11 17:56:24 +0000

    Lesson 17, chapter 3

--- zkSync/Lesson17/src/utils.js
+++ zkSync/Lesson17/src/utils.js
@@ -1,10 +1,27 @@
 async function getZkSyncProvider(zksync, networkName) {
-    let zkSyncProvider;
+    let zkSyncProvider
     try {
-        zkSyncProvider = await zksync.getDefaultProvider(networkName);
+        zkSyncProvider = await zksync.getDefaultProvider(networkName)
     } catch (error) {
-        console.log('Unable to connect to zkSync.');
-        console.log(error);
+        console.log('Unable to connect to zkSync.')
+        console.log(error)
     }
-    return zkSyncProvider;
+    return zkSyncProvider
+}
+
+async function getEthereumProvider(ethers, networkName) {
+    let ethersProvider
+    try {
+        // eslint-disable-next-line new-cap
+        ethersProvider = new ethers.getDefaultProvider(networkName)
+    } catch (error) {
+        console.log('Could not connect to Rinkeby')
+        console.log(error)
+    }
+    return ethersProvider
+}
+
+async function initAccount(rinkebyWallet, zkSyncProvider, zksync) {
+    const zkSyncWallet = await zksync.Wallet.fromEthSigner(rinkebyWallet, zkSyncProvider);
+    return zkSyncWallet;
 }
