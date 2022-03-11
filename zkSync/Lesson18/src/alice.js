b2202d282e0943f00ee4408fbdc41ed2f8bcf12f
K1-R1 <kyranrawlinson@gmail.com>
2022-03-11 19:05:12 +0000

    Lesson 18, chapter 2

--- zkSync/Lesson18/src/alice.js
+++ zkSync/Lesson18/src/alice.js
@@ -12,11 +12,12 @@
     console.log('Creating a new Rinkeby wallet for Alice')
     const aliceRinkebyWallet = new ethers.Wallet(process.env.ALICE_PRIVATE_KEY, ethersProvider)
     console.log(`Alice's Rinkeby address is: ${aliceRinkebyWallet.address}`)
-    const aliceInitialRinkebyBalance = await aliceRinkebyWallet.getBalance()
-    console.log(`Alice's initial balance on Rinkeby is: ${ethers.utils.formatEther(aliceInitialRinkebyBalance)}`)
-
     console.log('Creating a zkSync wallet for Alice')
     const aliceZkSyncWallet = await utils.initAccount(aliceRinkebyWallet, zkSyncProvider, zksync)
+
+    const tokenSet = zkSyncProvider.tokenSet
+    const aliceInitialRinkebyBalance = await aliceZkSyncWallet.getEthereumBalance(token)
+    console.log(`Alice's initial balance on Rinkeby is: ${tokenSet.formatToken(token, aliceInitialRinkebyBalance)}`)
 
     console.log('Depositing')
     await utils.depositToZkSync(aliceZkSyncWallet, token, amountToDeposit, ethers)
@@ -30,4 +31,5 @@
     console.log('Withdrawing')
     const withdrawalFee = await utils.getFee('Withdraw', aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
     await utils.withdrawToEthereum(aliceZkSyncWallet, amountToWithdraw, withdrawalFee, token, zksync, ethers)
+
 })()
